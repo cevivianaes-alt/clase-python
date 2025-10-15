c7fdc8dfa7571ff6bf3b5969d955d68aea01322c
ceviviana.es <ceviviana.es@tecnologicoargos.edu.ec>
2025-10-15 04:19:51 +0000

    TAREAS_SIST.

--- TAREA_DICCIONARIO.py
+++ TAREA_DICCIONARIO.py
@@ -1,17 +1,24 @@
 #ingresar inventario
 inventario = {"manzana": 10, "peras": 3, "naranjas": 8}
-print("Inventario inicial: ", inventario)
+print(inventario)
 
 #agregar un nuevo producto y cantidad
-inventario["kiwi"] = 7
-print("Agregar kiwi:", inventario)
+producto_nuevo = input("Ingrese el nombre del nuevo producto: ")
+cantidad_nueva = int(input("Ingrese la cantidad en stock:"))
+inventario[producto_nuevo] = cantidad_nueva
 
 # actualizar cantidad de kiwi
-inventario["kiwi"] = 9
-print("Cantidad actualizada de kiwi:", inventario)
+producto_actualizar = input("Ingrese el producto que desea actualizar:")
+if (producto_actualizar in inventario):
+    nueva_cantidad = int(input("Ingrese la nueva cantidad: "))
+        inventario[producto_actualizar] = nueva_cantidad
+        else:
+            print("El producto no existe en el inventario")
 
-#mostrar todos los productos en stock que sean menor a 5
-print("Productos con stock:")
-for producto, cantidad in inventario.items():
-    if cantidad < 5:
-        print(f"{producto}: {cantidad}")
+            print(inventario)
+
+            #mostrar todos los productos en stock que sean menor a 5
+            print("Productos con stock bajo:")
+            for producto, cantidad in inventario.items():
+                if cantidad < 5:
+                        print(f"{producto}: {cantidad}")
--- TAREA_TUPLA.py
+++ TAREA_TUPLA.py
@@ -2,15 +2,19 @@
 numeros = (5, 8, 12, 3, 9, 5, 2)
 
 #valor máximo y mínimo
-print("Valor Máximo: ", numeros[0])
-print("Valor Mínimo: ", numeros[6])
+maximo = max(numeros)
+minimo = min(numeros)
+print("Valor Máximo: ", maximo )
+print("Valor Mínimo: ", minimo )
 
 #Promedio de todos los valores
 promedio = sum(numeros) / len(numeros)
-print("Promedio de todos los valores: ", promedio)
+print("Promedio:", round(promedio, 2))
+#redund(promedio, 2) = redondear a dos dijitos
 
 #verificar si un número ingresado por el usuario está dentro de la tupla
 numero_ingresado = int(input("Ingrese un número: "))
 if numero_ingresado in numeros:
     print("El número ingresado está en la tupla.")
-
+else:
+    print("El numero no esta dentro de la tupla")
